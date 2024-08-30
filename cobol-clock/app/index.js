f11c021cd50149cb8b8be8e9012f891e3d4269bf
Joshua Horvath <jsh_197@hotmail.com>
2024-08-30 13:04:51 -0400

    Clock displays real time values

--- cobol-clock/app/index.js
+++ cobol-clock/app/index.js
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Joshua Horvath
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import clock from "clock";
 import * as document from "document";
 import { today as activity, minuteHistory } from "user-activity";
@@ -8,6 +32,12 @@ import { user } from "user-profile";
 // Update the clock every minute
 clock.granularity = "minutes";
 
+const hourLabel = document.getElementById("hourLabel");
+const minuteLabel = document.getElementById("minuteLabel");
+const amPmLabel = document.getElementById("amPmLabel");
+//const stepCountLabel = document.getElementById("stepCountLabel");
+//const floorsLabel = document.getElementById("floorsLabel");
+//const batteryLabel = document.getElementById("batteryLabel");
 
 clock.ontick = (evt) => {
     //console.log('Hours: ' + evt.date.getHours());
@@ -15,7 +45,44 @@ clock.ontick = (evt) => {
     //console.log('Day of week: ' + evt.date.getDay());  // zero based - Sunday == 0
     //console.log('Month: ' + (evt.date.getMonth() + 1));  // zero based - +1 to get expected number
     //console.log('Day of Month: ' + evt.date.getDate());  
+
+    updateTimeGroup(evt);
 }
+
+/**
+ * Updates the current time display values in GUI. 
+ * @param {*} evt 
+ */
+function updateTimeGroup(evt) {
+  // get time information from API
+  let todayDate = evt.date;
+  let rawHours = todayDate.getHours();
+
+  // 12 hour format
+  let hours = rawHours % 12 || 12;
+
+  let mins = todayDate.getMinutes();
+  let displayMins = zeroPad(mins);
+
+  // display time on main clock
+  hourLabel.text = `${hours}`;
+  minuteLabel.text = `${displayMins}`;
+
+  // AM / PM indicator
+  amPmLabel.text = rawHours >= 12 ? '"PM".' : '"AM".';
+}
+
+/**
+ * Front appends a zero to an integer if less than ten.
+ * @param {*} i 
+ * @returns 
+ */
+function zeroPad(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
 
 console.log('Hello ' + getTitle() + ' Cobol Clock user!');
 console.log('Battery: ' + battery.chargeLevel);
